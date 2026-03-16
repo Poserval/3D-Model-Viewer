a026f622119a7dc064f6c7ec46ab70775c7ce5a5
Poserval <140059835+Poserval@users.noreply.github.com>
2026-03-16 16:26:23 +1000

    Update MainActivity.java

--- android/app/src/main/java/com/yourcompany/modelviewer/MainActivity.java
+++ android/app/src/main/java/com/yourcompany/modelviewer/MainActivity.java
@@ -1,6 +1,7 @@
 package ru.dviewer.app;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
@@ -17,6 +18,11 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // ВКЛЮЧАЕМ ОТЛАДКУ ДЛЯ WEBVIEW
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
+
         webView = findViewById(R.id.webview);
         
         WebSettings webSettings = webView.getSettings();
@@ -31,7 +37,6 @@ public class MainActivity extends Activity {
         webSettings.setLoadWithOverviewMode(true);
         webSettings.setUseWideViewPort(true);
         
-        // Добавляем интерфейс для JavaScript
         webView.addJavascriptInterface(new WebAppInterface(), "Android");
         
         webView.setWebViewClient(new WebViewClient() {
@@ -59,10 +64,8 @@ public class MainActivity extends Activity {
         @JavascriptInterface
         public void saveFile(String base64Data, String fileName) {
             try {
-                // Декодируем base64
                 byte[] data = Base64.decode(base64Data, Base64.DEFAULT);
-                // Сохраняем через DownloadHelper
-                DownloadHelper.saveFile(MainActivity.this, data, fileName);
+                // DownloadHelper.saveFile(MainActivity.this, data, fileName);
             } catch (Exception e) {
                 e.printStackTrace();
             }
